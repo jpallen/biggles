3eae96a8ab27e309f666b3566c690616a63ae6a4
James Allen <james@jpallen.net>
2023-09-26 20:32:10 +0100

    Convert indentation to spaces consistently

--- src/extension.ts
+++ src/extension.ts
@@ -3,130 +3,130 @@ import OpenAI from "openai";
 import { adjustCode, createCode, getOpenAIClient, captureAudio } from './openai';
 
 export async function activate(context: vscode.ExtensionContext) {
-	const text = vscode.commands.registerCommand('biggles.text', () => biggles({voice: false}));
-	context.subscriptions.push(text);
+  const text = vscode.commands.registerCommand('biggles.text', () => biggles({voice: false}));
+  context.subscriptions.push(text);
 
-	const voice = vscode.commands.registerCommand('biggles.voice', () => biggles({voice: true}));
-	context.subscriptions.push(voice);
+  const voice = vscode.commands.registerCommand('biggles.voice', () => biggles({voice: true}));
+  context.subscriptions.push(voice);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
 
 type BigglesOptions = {
-	voice: boolean
+  voice: boolean
 };
 
 const biggles = async (options: BigglesOptions) => {
-	const openai = await getOpenAIClient();
-	if (!openai) { return; }
+  const openai = await getOpenAIClient();
+  if (!openai) { return; }
 
-	const editor = vscode.window.activeTextEditor;
-	if (!editor) {
-		return;
-	}
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
 
-	const selectedRange = getSelectedRange(editor);
+  const selectedRange = getSelectedRange(editor);
 
-	console.log('Selected range:', selectedRange.isEmpty);
+  console.log('Selected range:', selectedRange.isEmpty);
 
-	if (selectedRange && !selectedRange.isEmpty) {
-		await promptToEditCode(openai, editor, selectedRange, options);
-	} else {
-		await promptToInsertCode(openai, editor, options);
-	}
+  if (selectedRange && !selectedRange.isEmpty) {
+    await promptToEditCode(openai, editor, selectedRange, options);
+  } else {
+    await promptToInsertCode(openai, editor, options);
+  }
 };
 
 const promptToEditCode = async (openai: OpenAI, editor: vscode.TextEditor, selectedRange: vscode.Range, { voice }: BigglesOptions) => {
-	const instruction = await getInstruction(voice, "Modifications to selected code...", "Describe the modifications to the selected code");
-	if (!instruction) {
-		console.debug('No instruction given, aborting');
-		return;
-	}
+  const instruction = await getInstruction(voice, "Modifications to selected code...", "Describe the modifications to the selected code");
+  if (!instruction) {
+    console.debug('No instruction given, aborting');
+    return;
+  }
 
-	const leadingWhitespace = getIndentationOfCurrentLine(editor);
-	const selectedText = editor.document.getText(selectedRange);
-	const code = removeWhitespace(selectedText, leadingWhitespace);
+  const leadingWhitespace = getIndentationOfCurrentLine(editor);
+  const selectedText = editor.document.getText(selectedRange);
+  const code = removeWhitespace(selectedText, leadingWhitespace);
 
-	console.debug(`Code: ${code}`);
+  console.debug(`Code: ${code}`);
 
-	const adjustedCode = await adjustCode(openai, instruction, code);
+  const adjustedCode = await adjustCode(openai, instruction, code);
 
-	if (!adjustedCode) {
-		vscode.window.showErrorMessage("An error occurred. Please try again.");
-		return;
-	}
+  if (!adjustedCode) {
+    vscode.window.showErrorMessage("An error occurred. Please try again.");
+    return;
+  }
 
-	const indentInitialLine = selectedText.startsWith(leadingWhitespace);
-	const indentedCode = applyWhitespace(adjustedCode, leadingWhitespace, indentInitialLine);
-	console.debug(indentedCode);
+  const indentInitialLine = selectedText.startsWith(leadingWhitespace);
+  const indentedCode = applyWhitespace(adjustedCode, leadingWhitespace, indentInitialLine);
+  console.debug(indentedCode);
 
-	editor.edit(editBuilder => {
-		editBuilder.replace(selectedRange, indentedCode);
-	});
+  editor.edit(editBuilder => {
+    editBuilder.replace(selectedRange, indentedCode);
+  });
 };
 
 const promptToInsertCode = async (openai: OpenAI, editor: vscode.TextEditor, { voice }: BigglesOptions) => {
-	const instruction = await getInstruction(voice, "Description of code to insert...", "Describe the code you would like to insert");
-	if (!instruction) {
-		console.debug('No instruction given, aborting');
-		return;
-	}
-	
-	let doc = editor.document;
-	let pos = editor.selection.active;
-	let textToCursor = doc.getText(new vscode.Range(new vscode.Position(0, 0), pos));
-	let textAfterCursor = doc.getText(new vscode.Range(pos, new vscode.Position(doc.lineCount + 1, 0)));
+  const instruction = await getInstruction(voice, "Description of code to insert...", "Describe the code you would like to insert");
+  if (!instruction) {
+    console.debug('No instruction given, aborting');
+    return;
+  }
+  
+  let doc = editor.document;
+  let pos = editor.selection.active;
+  let textToCursor = doc.getText(new vscode.Range(new vscode.Position(0, 0), pos));
+  let textAfterCursor = doc.getText(new vscode.Range(pos, new vscode.Position(doc.lineCount + 1, 0)));
 
-	const code = await createCode(openai, instruction, textToCursor, textAfterCursor);
+  const code = await createCode(openai, instruction, textToCursor, textAfterCursor);
 
-	if (!code) {
-		vscode.window.showErrorMessage("An error occurred. Please try again.");
-		return;
-	}
+  if (!code) {
+    vscode.window.showErrorMessage("An error occurred. Please try again.");
+    return;
+  }
 
-	console.debug(`Code: ${code}`);
+  console.debug(`Code: ${code}`);
 
-	const leadingWhitespace = getIndentationOfCurrentLine(editor);
-	const indentedCode = applyWhitespace(code, leadingWhitespace, false);
-	console.debug(indentedCode);
+  const leadingWhitespace = getIndentationOfCurrentLine(editor);
+  const indentedCode = applyWhitespace(code, leadingWhitespace, false);
+  console.debug(indentedCode);
 
-	editor.edit(editBuilder => {
-		editBuilder.insert(editor.selection.active, indentedCode);
-	});
+  editor.edit(editBuilder => {
+    editBuilder.insert(editor.selection.active, indentedCode);
+  });
 };
 
 const getInstruction = async (voice: boolean, placeHolder: string, prompt: string) => {
-	let instruction: string | undefined;
-	if (voice) {
-		instruction = await captureAudio();
-	} else {
-		instruction = await vscode.window.showInputBox({
-			placeHolder,
-			prompt,
-		});
-	}
-	console.debug(`Instruction: ${instruction}`);
-	return instruction;
+  let instruction: string | undefined;
+  if (voice) {
+    instruction = await captureAudio();
+  } else {
+    instruction = await vscode.window.showInputBox({
+      placeHolder,
+      prompt,
+    });
+  }
+  console.debug(`Instruction: ${instruction}`);
+  return instruction;
 };
 
 const getSelectedRange = (editor: vscode.TextEditor) => {
-	const selection = editor.selection;
-	return new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+  const selection = editor.selection;
+  return new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
 };
 
 const getIndentationOfCurrentLine = (editor: vscode.TextEditor) => {
-	const document = editor.document;
-	const selection = editor.selection;
+  const document = editor.document;
+  const selection = editor.selection;
 
-	const currentLine = selection.start.line;
-	const currentLineText = document.lineAt(currentLine).text;
-	const whitespaceLength = currentLineText.length - currentLineText.trimStart().length;
-	const leadingWhitespace = currentLineText.slice(0, whitespaceLength);
+  const currentLine = selection.start.line;
+  const currentLineText = document.lineAt(currentLine).text;
+  const whitespaceLength = currentLineText.length - currentLineText.trimStart().length;
+  const leadingWhitespace = currentLineText.slice(0, whitespaceLength);
 
-	console.debug(`Leading whitespace: "${leadingWhitespace}"`);
-		
-	return leadingWhitespace;
+  console.debug(`Leading whitespace: "${leadingWhitespace}"`);
+    
+  return leadingWhitespace;
 };
 
 const applyWhitespace = (text: string, leadingWhitespace: string, indentInitialLine: boolean) => {
@@ -143,11 +143,11 @@ const applyWhitespace = (text: string, leadingWhitespace: string, indentInitialL
 const removeWhitespace = (text: string, leadingWhitespace: string) => {
   const lines = text.split('\n');
   const adjustedLines = lines.map((line, index) => {
-		if (line.startsWith(leadingWhitespace)) {
-			return line.slice(leadingWhitespace.length);
-		} else {
-			return line;
-		}
+    if (line.startsWith(leadingWhitespace)) {
+      return line.slice(leadingWhitespace.length);
+    } else {
+      return line;
+    }
   });
   return adjustedLines.join('\n');
 };
--- src/openai.ts
+++ src/openai.ts
@@ -7,168 +7,168 @@ import * as https from 'https';
 import { Readable } from 'node:stream';
 
 export const getOpenAIClient = async () => {
-	const { apiKey, organization, model } = getConfig();
-	if (typeof apiKey !== 'string' || apiKey.length === 0) {
-		const result = await vscode.window.showErrorMessage('No OpenAI API key found. Please add one to your settings.', 'Open Settings');
-		if (result === 'Open Settings') {
-			await vscode.commands.executeCommand('workbench.action.openSettings', 'biggles');
-		}
-		return;
-	}
+  const { apiKey, organization, model } = getConfig();
+  if (typeof apiKey !== 'string' || apiKey.length === 0) {
+    const result = await vscode.window.showErrorMessage('No OpenAI API key found. Please add one to your settings.', 'Open Settings');
+    if (result === 'Open Settings') {
+      await vscode.commands.executeCommand('workbench.action.openSettings', 'biggles');
+    }
+    return;
+  }
 
-	return new OpenAI({ apiKey, organization });
+  return new OpenAI({ apiKey, organization });
 };
 
 const getConfig = () => {
-	const config = vscode.workspace.getConfiguration('biggles');
-	const apiKey = config.get('openAI.apiKey') as string | undefined;
-	const organization = config.get('openAI.organization') as string | undefined;
-	const model = config.get('openAI.model') as string | undefined;
-	console.debug('initialised', { apiKey, organization });
-	return { apiKey, organization, model };
+  const config = vscode.workspace.getConfiguration('biggles');
+  const apiKey = config.get('openAI.apiKey') as string | undefined;
+  const organization = config.get('openAI.organization') as string | undefined;
+  const model = config.get('openAI.model') as string | undefined;
+  console.debug('initialised', { apiKey, organization });
+  return { apiKey, organization, model };
 };
 
 export const adjustCode = async (openai: OpenAI, instruction: string, code: string) => {
-	const { apiKey, organization, model } = getConfig();
-	const language = vscode.window.activeTextEditor?.document.languageId;
-	console.debug('Detected language (adjustCode):', language);
+  const { apiKey, organization, model } = getConfig();
+  const language = vscode.window.activeTextEditor?.document.languageId;
+  console.debug('Detected language (adjustCode):', language);
 
-	return vscode.window.withProgress({
-		location: vscode.ProgressLocation.Notification,
-		title: instruction
-	}, async () => {
-		const completion = await openai.chat.completions.create({
-			messages: [
-				{
-					role: "system",
-					content: `You are a coding assistent for the ${language} programming language. The user prompt will be an instruction and a snippet of code to adjust. The output will be the adjusted code with no introduction or prefix.`
-				},
-				{
-					role: "user",
-					content: `Instruction: ${instruction}\nCode:\n${code}`
-				}
-			],
-			model: model || "gpt-4",
-		});
+  return vscode.window.withProgress({
+    location: vscode.ProgressLocation.Notification,
+    title: instruction
+  }, async () => {
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: `You are a coding assistent for the ${language} programming language. The user prompt will be an instruction and a snippet of code to adjust. The output will be the adjusted code with no introduction or prefix.`
+        },
+        {
+          role: "user",
+          content: `Instruction: ${instruction}\nCode:\n${code}`
+        }
+      ],
+      model: model || "gpt-4",
+    });
 
-		const content = completion.choices[0]?.message.content;
-		return content;
-	});
+    const content = completion.choices[0]?.message.content;
+    return content;
+  });
 };
 
 export const createCode = async (openai: OpenAI, instruction: string, textBeforeCursor: string, textAfterCursor: string) => {
-	const language = vscode.window.activeTextEditor?.document.languageId;
-	console.debug('Detected language (createCode):', language);
+  const language = vscode.window.activeTextEditor?.document.languageId;
+  console.debug('Detected language (createCode):', language);
 
-	return vscode.window.withProgress({
-		location: vscode.ProgressLocation.Notification,
-		title: instruction
-	}, async () => {
-		const messages: OpenAI.Chat.ChatCompletionMessageParam[] = [
-			{
-				role: "system",
-				content: `You are a coding assistent for the ${language} programming language. The user prompt will the code before the cursor in <before> tags, the code after the cursor in <after> tags and a description of the new code to insert in <instruction> tags. The output will be code to insert between the before and after code which does what the user description says with no instroduction or explanation. `
-			},
-			{
-				role: "user",
-				content: `<instruction>${instruction}</instruction>\n<before>${textBeforeCursor}</before>\n<after>${textAfterCursor}</after>`
-			}
-		];
-		const completion = await openai.chat.completions.create({
-			messages: messages,
-			model: "gpt-4",
-		});
+  return vscode.window.withProgress({
+    location: vscode.ProgressLocation.Notification,
+    title: instruction
+  }, async () => {
+    const messages: OpenAI.Chat.ChatCompletionMessageParam[] = [
+      {
+        role: "system",
+        content: `You are a coding assistent for the ${language} programming language. The user prompt will the code before the cursor in <before> tags, the code after the cursor in <after> tags and a description of the new code to insert in <instruction> tags. The output will be code to insert between the before and after code which does what the user description says with no instroduction or explanation. `
+      },
+      {
+        role: "user",
+        content: `<instruction>${instruction}</instruction>\n<before>${textBeforeCursor}</before>\n<after>${textAfterCursor}</after>`
+      }
+    ];
+    const completion = await openai.chat.completions.create({
+      messages: messages,
+      model: "gpt-4",
+    });
 
-		const content = completion.choices[0]?.message.content;
-		return content;
-	});
+    const content = completion.choices[0]?.message.content;
+    return content;
+  });
 };
 
 export const captureAudio = async () => {
-	const { apiKey, organization } = getConfig();
+  const { apiKey, organization } = getConfig();
 
-	let stream: Readable;
-	try {
-		stream = await rec.start();
-	} catch (error) {
-		if ((error as any).code === 'ENOENT') {
-			const result = await vscode.window.showErrorMessage('Failed to start recording: SoX not found', { modal: true }, 'Install SoX');
-			if (result === 'Install SoX') {
-				vscode.env.openExternal(vscode.Uri.parse('https://github.com/jpallen/biggles#sox'));
-			}
-			return;
-		} else {
-			throw error;
-		}
-	}
+  let stream: Readable;
+  try {
+    stream = await rec.start();
+  } catch (error) {
+    if ((error as any).code === 'ENOENT') {
+      const result = await vscode.window.showErrorMessage('Failed to start recording: SoX not found', { modal: true }, 'Install SoX');
+      if (result === 'Install SoX') {
+        vscode.env.openExternal(vscode.Uri.parse('https://github.com/jpallen/biggles#sox'));
+      }
+      return;
+    } else {
+      throw error;
+    }
+  }
 
-	const formData = new FormData();
-	formData.append('model', 'whisper-1');
-	const boundary = formData.getBoundary();
-	formData.append('file', stream, { contentType: 'audio/wav', filename: 'audio.wav' });
+  const formData = new FormData();
+  formData.append('model', 'whisper-1');
+  const boundary = formData.getBoundary();
+  formData.append('file', stream, { contentType: 'audio/wav', filename: 'audio.wav' });
 
-	const headers: Record<string, string> = {
-		'Authorization': `Bearer ${apiKey}`,
-		'Content-Type': `multipart/form-data; boundary=${boundary}`,
-	};
+  const headers: Record<string, string> = {
+    'Authorization': `Bearer ${apiKey}`,
+    'Content-Type': `multipart/form-data; boundary=${boundary}`,
+  };
 
-	if (organization) {
-		headers['OpenAI-Organization'] = organization;
-	}
+  if (organization) {
+    headers['OpenAI-Organization'] = organization;
+  }
 
-	const options = {
-		hostname: 'api.openai.com',
-		path: '/v1/audio/transcriptions',
-		method: 'POST',
-		headers
-	};
+  const options = {
+    hostname: 'api.openai.com',
+    path: '/v1/audio/transcriptions',
+    method: 'POST',
+    headers
+  };
 
-	// TODO: Handle OpenAI error and cancel recording
-	return new Promise<string>((resolveRequest, reject) => {
-		vscode.window.withProgress({
-			location: vscode.ProgressLocation.Notification,
-			title: "Talk to me...",
-			cancellable: true
-		}, (progress, token) => {
-			let cancelled = false;
-			token.onCancellationRequested(() => {
-				rec.stop();
-				cancelled = true;
-				console.log("User cancelled the recording");
-			});
+  // TODO: Handle OpenAI error and cancel recording
+  return new Promise<string>((resolveRequest, reject) => {
+    vscode.window.withProgress({
+      location: vscode.ProgressLocation.Notification,
+      title: "Talk to me...",
+      cancellable: true
+    }, (progress, token) => {
+      let cancelled = false;
+      token.onCancellationRequested(() => {
+        rec.stop();
+        cancelled = true;
+        console.log("User cancelled the recording");
+      });
 
-			return new Promise<void>(resolveProgress => {
-				const req = https.request(options, (res) => {
-					let body = '';
+      return new Promise<void>(resolveProgress => {
+        const req = https.request(options, (res) => {
+          let body = '';
 
-					res.on('data', (chunk) => {
-						body += chunk;
-					});
+          res.on('data', (chunk) => {
+            body += chunk;
+          });
 
-					res.on('end', () => {
-						resolveProgress();
-						if (cancelled) {
-							reject(new Error('user cancelled'));
-						}
-						try {
-							const parsedBody = JSON.parse(body);
-							if (parsedBody.text) {
-								resolveRequest(parsedBody.text as string);
-							} else {
-								reject(new Error('Text field does not exist in the response.'));
-							}
-						} catch (error) {
-							reject(error);
-						}
-					});
+          res.on('end', () => {
+            resolveProgress();
+            if (cancelled) {
+              reject(new Error('user cancelled'));
+            }
+            try {
+              const parsedBody = JSON.parse(body);
+              if (parsedBody.text) {
+                resolveRequest(parsedBody.text as string);
+              } else {
+                reject(new Error('Text field does not exist in the response.'));
+              }
+            } catch (error) {
+              reject(error);
+            }
+          });
 
-					res.on('error', reject);
-				});
+          res.on('error', reject);
+        });
 
-				formData.pipe(req);
+        formData.pipe(req);
 
-				req.on('error', reject);
-			});
-		});
-	});
+        req.on('error', reject);
+      });
+    });
+  });
 };
