65dfc103a9796ddccd29516b782a61cc4dc2277d
Josh Cowling <j.j.cowling1@gmail.com>
2023-09-26 12:51:05 +0100

    added OS setting (WIP replace with some test?) added shortcut for text input

--- src/record.ts
+++ src/record.ts
@@ -25,11 +25,21 @@
 
 import { spawn, ChildProcess } from 'child_process';
 import { Readable } from 'node:stream';
+import * as vscode from 'vscode';
 
 let cp: ChildProcess | null; // Recording process
 
+const getConfig = () => { 
+	const config = vscode.workspace.getConfiguration('biggles');
+	const os = config.get('system.os') as string | undefined;
+	console.debug('initialised', {os});
+  return {os};
+};
+
 export function start(userOptions: Partial<RecordOptions> = {}): Promise<Readable> {
   cp = null; // Empty out possibly dead recording process
+
+  const {os} = getConfig();
 
   const defaults: RecordOptions = {
     sampleRate: 16000,
@@ -44,24 +54,43 @@ export function start(userOptions: Partial<RecordOptions> = {}): Promise<Readabl
     device: null
   };
 
-  const options:RecordOptions = Object.assign(defaults, userOptions);
+  const options: RecordOptions = Object.assign(defaults, userOptions);
 
   const cmd = "sox";
   let audioType = "wav";
-  if (options.audioType) {audioType = options.audioType;};
-  const cmdArgs = [
-    '-d',
-    '-t', audioType,          // audio type
-    '-q',                     // show no progress
-    '-r', options.sampleRate.toString(), // sample rate
-    '-c', options.channels.toString(),   // channels
-    '-e', 'signed-integer',   // sample encoding
-    '-b', '16',               // precision (bits)
-    '-',                      // pipe
-        // end on silence
-    'silence', '1', '0.1', (options.thresholdStart || options.threshold + '%').toString(),
-    '1', options.silence, (options.thresholdEnd || options.threshold + '%').toString()
-  ];
+  if (options.audioType) { audioType = options.audioType; };
+
+  var cmdArgs;
+
+  if (os === "win") {
+    cmdArgs = [
+      '-q',                                   // show no progress
+      '-t', 'waveaudio',
+      '-d',
+      '-r', options.sampleRate.toString(),    // sample rate
+      '-c', '1',                              // channels
+      '-e', 'signed-integer',                 // sample encoding
+      '-b', '16',                             // precision (bits)
+      '-t', audioType,  // audio type
+      '-',
+      'silence', '1', '0.1', (options.thresholdStart || options.threshold + '%').toString(),
+      '1', options.silence, (options.thresholdEnd || options.threshold + '%').toString()
+    ];
+  } else {
+    cmdArgs = [
+      '-d',
+      '-t', audioType,          // audio type
+      '-q',                     // show no progress
+      '-r', options.sampleRate.toString(), // sample rate
+      '-c', options.channels.toString(),   // channels
+      '-e', 'signed-integer',   // sample encoding
+      '-b', '16',               // precision (bits)
+      '-',                      // pipe
+      // end on silence
+      'silence', '1', '0.1', (options.thresholdStart || options.threshold + '%').toString(),
+      '1', options.silence, (options.thresholdEnd || options.threshold + '%').toString()
+    ];
+  }
 
   // Spawn audio capture command
   const cmdOptions: Record<string, any> = { encoding: 'binary' };
@@ -75,7 +104,7 @@ export function start(userOptions: Partial<RecordOptions> = {}): Promise<Readabl
 
   if (options.verbose) {
     console.log('Recording', options.channels, 'channels with sample rate',
-        options.sampleRate + '...');
+      options.sampleRate + '...');
     console.time('End Recording');
 
     rec.on('data', function (data) {
